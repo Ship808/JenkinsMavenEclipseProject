a7f29cfab5d449642c357b71f7b1b1bb8ad7b148
ship808 <shiprasingh191998@gmail.com>
2021-04-21 16:06:44 +0530

    Jenkins Maven Project Updated

---
+++ src/main/java/com/coforge/jenkinsMavenEx/PlayerMain.java
@@ -0,0 +1,12 @@
+package com.coforge.jenkinsMavenEx;
+
+
+
+public class PlayerMain {
+
+	public static void main(String args[])
+	{
+		Player sachin= new Player(10, "Sachin tendulkar", "maharashtra");
+		System.out.println(sachin.getJerseyNo()+" : "+sachin.getPlayerName()+" : "+sachin.getState());
+	}
+}
